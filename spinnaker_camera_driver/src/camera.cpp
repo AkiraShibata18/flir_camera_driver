adcc428b1f5ac5f925e4c57e1244cf13c4cfdbbf
AkiraShibata18 <a.shibata@gitai.tech>
2022-06-01 18:58:09 +0900

    add auto exposure lower limit to dynamic reconfigure

--- spinnaker_camera_driver/src/camera.cpp
+++ spinnaker_camera_driver/src/camera.cpp
@@ -139,6 +139,8 @@ void Camera::setNewConfiguration(SpinnakerConfig& config, const uint32_t& level)
     {
       setProperty(node_map_, "AutoExposureExposureTimeUpperLimit",
                   static_cast<float>(config.auto_exposure_time_upper_limit));
+      setProperty(node_map_, "AutoExposureExposureTimeLowerLimit",
+                  static_cast<float>(config.auto_exposure_time_lower_limit));
     }
     Spinnaker::GenApi::CFloatPtr ptrExposureTime = node_map_->GetNode("ExposureTime");
     config.exposure_time = ptrExposureTime->GetValue();
